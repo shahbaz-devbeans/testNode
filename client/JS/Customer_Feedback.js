3dbe0d1946fff0fbbcb8e6881ba53629d840abfe
Hira-icode <hirajamshed6@gmail.com>
2021-06-13 02:34:40 +0500

    changes added to server and feedback js

--- client/JS/Customer_Feedback.js
+++ client/JS/Customer_Feedback.js
@@ -1,9 +1,8 @@
 onload = () => {
-    const formContact = document.getElementById("customer_feedback_form");
+    const formFeedback = document.getElementById("customer_feedback_form");
     const yourName = document.getElementById("name");
     const yourEmail = document.getElementById("email");
-    const phoneNumber = document.getElementById("number");
-    const message = document.getElementById("message");
+    const comments = document.getElementById("comments");
 
   
     function isFormValid() {
@@ -11,28 +10,23 @@ onload = () => {
       if (
         yourName.value === "" ||
         yourEmail.value === "" ||
-        phoneNumber.value === "" ||
-        message.value === ""
+        comments.value === ""
       ) {
         alert("Please fill all the fields");
       } else if (yourName.value.length > 30) {
         alert("PLease check the length of the characters");
-      } else if (phoneNumber.value.length !== 10) {
-        alert("Please check length of the mobile number");
-      } else if (isNaN(phoneNumber.value)) {
-        alert("Please check the number. It should be only number");
-      } else if (message.value.length > 300) {
-        alert("Please check the maximum length of characters");
+      } else if (comments.value.length < 30) {
+        alert("Please check the minimum length of characters");
       } else {
         isValid = true;
         alert(
-          `Your Name: ${yourName.value} \n Your Email: ${yourEmail.value} \n Your Phone NUmber: ${phoneNumber.value} \n Your Message: ${message.value}`
+          `Your Name: ${yourName.value} \n Your Email: ${yourEmail.value} \n Your Comments: ${comments.value}`
         );
       }
       return isValid;
     }
   
-    formContact.onsubmit = async (e) => {
+    formFeedback.onsubmit = async (e) => {
       e.preventDefault();
 
       console.log("in form submit")
@@ -40,7 +34,7 @@ onload = () => {
         // This is essentially getting all the form data and making like and object
         // such as:
         // {name: "ygor", email: "myemail.com"} and so on
-        const formData = new URLSearchParams(new FormData(formContact));
+        const formData = new URLSearchParams(new FormData(formFeedback));
   
         let response = await fetch("http://localhost:3001/api/CustomerFeedback", {
           method: "POST",
